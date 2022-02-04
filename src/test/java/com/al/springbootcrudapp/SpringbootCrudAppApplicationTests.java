cbe02a7ae9c1572a1990c9623d3d378c70abc313
Tarik <taryk.haroun@gmail.com>
2022-02-04 15:19:22 +0100

    initial commit

---
+++ src/test/java/com/al/springbootcrudapp/SpringbootCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.al.springbootcrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
